db7c33cbf48051e5a9ef07590ba6b29e11ba095e
Marco Pinheiro <mpinheiro19@gmail.com>
2022-11-02 12:21:24 -0300

    first commit

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,31 @@
+terraform {
+
+    required_version = "1.3.3"
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        versionversion = "4.37.0"
+      }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+    bucket = "mpinheiro19-my-tf-test-bucket"
+   
+    tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby = "Terraform"
+    }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+    bucket = aws_s3_bucket.b.id
+    acl = "private"
+
+}
