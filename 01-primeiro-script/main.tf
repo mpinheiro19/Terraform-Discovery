ba2bdc2dc690fc75ac8206d090f1e0418ad1a564
Marco Pinheiro <mpinheiro19@gmail.com>
2022-11-02 17:29:50 -0300

    adding alter and destroy Terraform commands

--- 01-primeiro-script/main.tf
+++ 01-primeiro-script/main.tf
@@ -5,7 +5,7 @@ terraform {
     required_providers {
       aws = {
         source = "hashicorp/aws"
-        versionversion = "4.37.0"
+        version = "4.37.0"
       }
     }
 }
@@ -18,9 +18,13 @@ resource "aws_s3_bucket" "b" {
     bucket = "mpinheiro19-my-tf-test-bucket"
    
     tags = {
+
     Name        = "My bucket"
+
     Environment = "Dev"
+
     Managedby = "Terraform"
+
     }
 }
 
