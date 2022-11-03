9927ba1612ac0aa0f37da4d23fa24fecea030677
Marco Pinheiro <mpinheiro19@gmail.com>
2022-11-03 19:46:01 -0300

    adding interpolation locals output codes to folder

---
+++ 04-interpolation-locals-outputs/s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "this" {
+
+  bucket = "${random_pet.bucket.id}-${var.environment}"
+
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+
+}
+
+resource "aws_s3_object" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  key = "config/${local.payload_object}"
+
+  source = "${local.payload_folder}/${local.payload_object}"
+
+  etag = filemd5("${local.payload_folder}/${local.payload_object}")
+
+}
